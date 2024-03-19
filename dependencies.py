317c130194d6f84e93a78448928001a5040dcc12
Kamil Kondziolka <wono83@interia.pl>
2024-03-19 13:08:45 +0100

    Fixed dependencies.py

--- dependencies.py
+++ dependencies.py
@@ -3,8 +3,8 @@ import zipfile
 import os
 
 mega_url = 'https://mega.nz/file/JFIzAIKT#hg5xK3rk6RmoxFqZwV0qsdg0N7zQQr-6qSSas4fNusg'
-zip_file_path = './dependencies.zip'
-extract_path = './'
+zip_file_path = './use_model.zip'
+extract_path = './ConditioningAugmentation'
 
 mega = Mega()
 m = mega.login()
@@ -18,9 +18,9 @@ if not os.path.exists(zip_file_path):
         pass
 
 # Check if the folder where you want to extract the zip file exists
-if not os.path.exists(os.path.join(extract_path, 'Data', 'CUB_200_2011_reformat', 'images')):
+if not os.path.exists(os.path.join(extract_path, 'ConditioningAugmentation', 'saved_model.pb')):
     print("Extracting...")
     with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
         zip_ref.extractall(extract_path)
 else:
-    print("Folder already exists. Skipping extraction.")
+    print("Model file already exists")
